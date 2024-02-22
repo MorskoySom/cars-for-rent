bffc452b057a31c804dae9270960905711a3b4a9
MorskoySom <Somovvitvit@gmail.com>
2024-02-22 03:53:17 +0200

    b2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename='/cars-for-rent'>
        <App />
      </BrowserRouter>
     </React.StrictMode>
